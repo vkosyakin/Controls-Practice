6b729eb177bfc54eae91e7acb16e609832060f86
Valeriy Kosyakin <valeradzu@mail.ru>
2021-06-16 12:09:56 +0300

    Initial Commit

---
+++ Controls Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Controls Practice
+//
+//  Created by Валера on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
