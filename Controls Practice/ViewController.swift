812e32401e73502df7329efdfeb0a492b400bcbd
Valeriy Kosyakin <valeradzu@mail.ru>
2021-06-18 14:22:58 +0300

    implemented number for button ,slider, and tex fleid

--- Controls Practice/ViewController.swift
+++ Controls Practice/ViewController.swift
@@ -9,29 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var Button: UIButton!
-    
+    @IBOutlet weak var button: UIButton!
     @IBOutlet var switches: [UISwitch]!
-    
     @IBOutlet weak var slider: UISlider!
-    
     @IBOutlet weak var textField: UITextField!
+    
+    var number: UInt8 = 128 {
+        didSet{
+            updateUI()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       updateUI()
+    }
+    /// Updates all outlets to number
+    func updateUI(){
+        button.setTitle("\(number)", for: [])
+        
+   // TODO: set switches to number
+        slider.value = Float(number)
+        textField.text = "\(number)"
     }
      
     @IBAction func buttonPressed() {
+        number = UInt8((Int(number) + 1) % 256)
     }
     
     @IBAction func switchToggled(_ sender: UISwitch) {
+        print(#line,#function,sender.tag)
     }
     
     @IBAction func sliderMoved() {
+        number = UInt8(slider.value)
     }
     
     @IBAction func textFieldEdited() {
+        number = UInt8(textField.text ?? "") ?? 128
+        
     }
+    
 }
 
